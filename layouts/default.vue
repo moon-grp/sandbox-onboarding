f73ae1e8f98f068fff39ce9d3ea514269ed9c486
moon-grp <olumidemm@gmail.com>
2020-02-21 09:48:17 +0100

    navbar v1

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,82 +7,78 @@
       fixed
       app
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app flat color="#FFFFFF">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
+       <v-toolbar-title>
+          <v-img  src="/logo.svg" height="50px" width="120px">
+          </v-img>
+       </v-toolbar-title>
+      
+         
+        
+        <v-spacer />
+        <hr>
+            <v-icon class="mr-3" large color="green darken-2">mdi-magnify</v-icon>
+               <v-btn
+        depressed
+        >
+             <v-badge
+          color="#007BFF"
+          content="6"
+          overlap
+        >
+     
+          <v-icon  large color="green darken-2">mdi-email-outline</v-icon>
+          </v-badge>
+          Messages
+        </v-btn>
+
+                  <v-btn
+        depressed
+        >
+             <v-badge
+          color="#007BFF"
+          content="6"
+          overlap
+        >
+     
+          <v-icon class="mr-3" large color="green darken-2">mdi-cart-outline</v-icon>
+          </v-badge>
+          Market place
+        </v-btn>
+
+           <v-btn
+        depressed
+        >
+             <v-badge
+          color="#007BFF"
+          content="6"
+          overlap
+        >
+     
+          <v-icon class="mr-3" large color="green darken-2">mdi-menu</v-icon>
+          </v-badge>
+          Notifications
+        </v-btn>
+        
+         <v-avatar>
+      <img
+        src="https://cdn.vuetifyjs.com/images/john.jpg"
+        alt="John"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+    </v-avatar>
+    <div>
+      Me
+    </div>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+
+    <v-footer :fixed="fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -90,28 +86,28 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      title: "Vuetify.js"
+    };
   }
-}
+};
 </script>
